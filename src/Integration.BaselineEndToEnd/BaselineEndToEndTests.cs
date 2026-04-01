a7d886ddeec141caa90d6144e4301593fd97e6c1
marineKP52 <gls.maryna@gmail.com>
2026-04-01 23:24:16 +0300

    Integration tests project created.

---
+++ src/Integration.BaselineEndToEnd/BaselineEndToEndTests.cs
@@ -0,0 +1,63 @@
+﻿public class BaselineEndToEndTests
+{
+    [Test]
+    public void TrigramOnRealCorpus_GeneratesUkrainianWords()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    [Test]
+    public void FullCheckpoint_SaveLoad_SameGeneration()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    [Test]
+    public void WordAndCharTokenizer_BothProduceReadableOutput()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    [Test]
+    public void Trigram_Perplexity_OnValidation_IsFinite()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    [Test]
+    public void Trigram_HasLowerPerplexity_ThanBigram()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    [Test]
+    public void NextTokenScores_SumsToOne_ForAllTokens()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
+
